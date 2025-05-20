7cbb4881cd843da1e18812027aad0125c27bd6e9
Ivan Kastle <bluehorizon186@gmail.com>
2025-05-19 18:42:48 -0700

    Learned how to make a self-resizing canvas!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* General Configuration! */
+
 const optionsPanel = document.getElementById("optionspanel");
 const toggleBtn = document.getElementById("togglebtn");
 const workspace = document.getElementById("workspace");
@@ -18,3 +20,40 @@ toggleBtn.addEventListener("click", () => {
         workspace.style.marginLeft = "30%";
     }
 });
+
+/* Canvas Configuration! */
+
+const DEFAULT_WIDTH = 1920;
+const DEFAULT_HEIGHT = 1080;
+
+const widthInput = document.getElementById("width-txtbx");
+const heightInput = document.getElementById("height-txtbx");
+const canvas = document.getElementById("thecanvas");
+
+resizeCanvas(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+
+widthInput.addEventListener("input", updateCanvas);
+heightInput.addEventListener("input", updateCanvas);
+
+function updateCanvas()
+{
+    let newWidth = widthInput.value || DEFAULT_WIDTH;
+    let newHeight = heightInput.value || DEFAULT_HEIGHT;
+
+    if (newWidth <= 0)
+        newWidth = 1;
+
+    if (newHeight <= 0)
+        newHeight = 1;
+
+    resizeCanvas(newWidth, newHeight);
+}
+
+function resizeCanvas(width, height)
+{
+    canvas.width = width;
+    canvas.height = height;
+
+    document.documentElement.style.setProperty("--canvas-width", width + "px");
+    document.documentElement.style.setProperty("--canvas-height", height + "px");
+}
